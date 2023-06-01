fd54aecde1170c67db9691b495c7a6bf87031ad6
Jay H <mobeets@gmail.com>
2023-06-01 10:37:31 -0400

    uncommenting

--- bin/run.sh
+++ bin/run.sh
@@ -5,15 +5,15 @@ DATADIR='data/models'
 DO_ANALYZE=true
 
 # Starkweather Task 1
-# if $DO_ANALYZE
-# then
-# 	python analyze.py -e starkweather-task1 -m pomdp -i $DATADIR
-# 	python analyze.py -e starkweather-task1 -m value-rnn-trained -i $DATADIR
-# 	python analyze.py -e starkweather-task1 -m value-rnn-untrained -i $DATADIR
-# fi
-# python plot.py -e starkweather-task1 -i $DATADIR
+if $DO_ANALYZE
+then
+	python analyze.py -e starkweather-task1 -m pomdp -i $DATADIR
+	python analyze.py -e starkweather-task1 -m value-rnn-trained -i $DATADIR
+	python analyze.py -e starkweather-task1 -m value-rnn-untrained -i $DATADIR
+fi
+python plot.py -e starkweather-task1 -i $DATADIR
 
-# # Starkweather Task 2
+# Starkweather Task 2
 if $DO_ANALYZE
 then
 	python analyze.py -e starkweather-task2 -m pomdp -i $DATADIR
@@ -24,10 +24,10 @@ fi
 python plot.py -e starkweather-task2 -i $DATADIR
 
 # Babayan task
-# if $DO_ANALYZE
-# then
-# 	python analyze.py -e babayan -m pomdp -i $DATADIR
-# 	python analyze.py -e babayan -m value-rnn-trained -i $DATADIR
-# 	python analyze.py -e babayan -m value-rnn-untrained -i $DATADIR
-# fi
-# python plot.py -e babayan -i $DATADIR
+if $DO_ANALYZE
+then
+	python analyze.py -e babayan -m pomdp -i $DATADIR
+	python analyze.py -e babayan -m value-rnn-trained -i $DATADIR
+	python analyze.py -e babayan -m value-rnn-untrained -i $DATADIR
+fi
+python plot.py -e babayan -i $DATADIR
